d1bcb4c64a76e9fb030b63aa0d75246609598d7e
m-hayakata <m-hayakata@aspire.co.jp>
2016-10-17 00:21:43 +0900

    整理

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -1,10 +1,6 @@
 package com.example
 
 import com.example.controller.GitHubController
-import org.json4s.DefaultFormats
-import org.json4s.native.JsonMethods
-
-import scala.io.Source
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -13,38 +9,4 @@ object Main {
 
     new GitHubController
   }
-}
-
-case class Settings(gitHubId: String, gitHubPassword: String)
-
-object Settings {
-  // TODO catch FileNotFoundException
-  private val jsonText = Source.fromFile(Const.PATH_SETTINGS_FILE)
-    .getLines()
-    // コメントアウト行の除外
-    .filter(!_.toString.trim.startsWith("//"))
-    .mkString
-
-  // Brings in default date formats etc.
-  private implicit val formats = DefaultFormats
-  private val values = JsonMethods.parse(jsonText).extract[Values]
-  val gitHub: GitHub = values.gitHub
-  val pullreq: Pullreq = values.pullreq
-
-  /**
-    * for json extract
-    */
-  case class Values(gitHub: GitHub, pullreq: Pullreq)
-
-  case class GitHub(id: String, password: String)
-
-  case class Pullreq(
-      taskKeyRegexFormat: String,
-      ignoreTaskKeyList: List[String],
-      labelWaitingMerge: String,
-      labelWaitingFix: String,
-      labelUnusedList: List[String],
-      labelReviewerList: List[String]
-  )
-
 }
---
+++ src/main/scala/com/example/Settings.scala
@@ -0,0 +1,40 @@
+package com.example
+
+import org.json4s.DefaultFormats
+import org.json4s.native.JsonMethods
+
+import scala.io.Source
+
+case class Settings(gitHubId: String, gitHubPassword: String)
+
+object Settings {
+  // TODO catch FileNotFoundException
+  private val jsonText = Source.fromFile(Const.PATH_SETTINGS_FILE)
+    .getLines()
+    // コメントアウト行の除外
+    .filter(!_.toString.trim.startsWith("//"))
+    .mkString
+
+  // Brings in default date formats etc.
+  private implicit val formats = DefaultFormats
+  private val values = JsonMethods.parse(jsonText).extract[Values]
+  val gitHub: GitHub = values.gitHub
+  val pullreq: Pullreq = values.pullreq
+
+  /**
+    * for json extract
+    */
+  case class Values(gitHub: GitHub, pullreq: Pullreq)
+
+  case class GitHub(id: String, password: String)
+
+  case class Pullreq(
+      taskKeyRegexFormat: String,
+      ignoreTaskKeyList: List[String],
+      labelWaitingMerge: String,
+      labelWaitingFix: String,
+      labelUnusedList: List[String],
+      labelReviewerList: List[String]
+  )
+
+}
