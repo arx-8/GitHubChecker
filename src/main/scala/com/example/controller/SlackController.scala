d7d00dc0c697c402ed16763d5508cef6bf7cc9d7
m-hayakata <m-hayakata@aspire.co.jp>
2016-10-18 07:48:14 +0900

    sendMessageできるとこまで

--- src/main/scala/com/example/controller/SlackController.scala
+++ src/main/scala/com/example/controller/SlackController.scala
@@ -5,14 +5,24 @@ import com.ullink.slack.simpleslackapi.impl.SlackSessionFactory
 
 class SlackController {
   private val session = SlackSessionFactory.createWebSocketSlackSession(Settings.slack.apiToken)
-  session.connect()
 
-  private val channel = Option(session.findChannelByName(Settings.slack.postChannel))
-  channel match {
-    case Some(c) => session.sendMessage(c, "Hello world")
-    case None => throw new IllegalStateException(Settings.slack.postChannel + "is not found.")
+  /**
+    * loan-pattern ?
+    * http://xerial.org/scala-cookbook/recipes/2012/06/27/loan-pattern/
+    *
+    * @param msg
+    */
+  def sendMessage(msg: String): Unit = {
+    session.connect()
+
+    val channel = Option(session.findChannelByName(Settings.slack.postChannel))
+    channel match {
+      case Some(c) => session.sendMessage(c, msg)
+      case None => throw new IllegalStateException(Settings.slack.postChannel + "is not found.")
+      // TODO チャンネル名は設定から取ってるので、もっと親切に
+    }
+
+    // 注意：disconnectしないと常駐する
+    session.disconnect()
   }
-
-  // 注意：disconnectしないと常駐する
-  session.disconnect()
 }
